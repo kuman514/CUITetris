b71e492cdb946ccbbd6b15f82390bdbdbcbcdc5d
kumankoishi.py <7iggif@naver.com>
2020-04-16 11:39:31 +0900

    Implemented Bricks Falling

--- CUITetris/CUITetris.cpp
+++ CUITetris/CUITetris.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 #include <cstdio>
+#include <cstdlib>
+#include <ctime>
 #include <conio.h>
 #include <Windows.h>
 
@@ -37,11 +39,17 @@ bool tetromino[19][4][4] = { {{1,0,0,0},{1,0,0,0},{1,0,0,0},{1,0,0,0}}, {{1,1,1,
 // [start, end)
 int tetIndex[7][2] = { {0, 2}, {2, 3}, {3, 5}, {5, 7}, {7, 11}, {11, 15}, {15, 19} };
 const int maxY = 20, maxX = 10;
-bool bricks[maxY][maxX];
+bool bricks[maxY + 4][maxX + 4];
 
 int frame;
-int line;
 
+int line;
+int xpos;
+
+int curblock;
+int curturn;
+
+void init(void);
 void gotoxy(const int, const int);
 void invalidate(void);
 
@@ -54,6 +62,8 @@ void countframe(void);
 
 int main(void)
 {
+    init();
+
     while (true)
     {
         // process
@@ -73,6 +83,23 @@ int main(void)
     }
 
     return 0;
+}
+
+void init(void)
+{
+    srand(time(NULL));
+    curblock = rand() % 7;
+
+    line = 0;
+    xpos = 3;
+
+    for (int y = 0; y < 4; y++)
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            bricks[line + y][xpos + x] = tetromino[tetIndex[curblock][curturn]][y][x];
+        }
+    }
 }
 
 void gotoxy(const int x, const int y)
@@ -108,13 +135,40 @@ void fall(void)
 {
     if (frame >= 59)
     {
+        for (int y = 0; y < 4; y++)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                if (tetromino[tetIndex[curblock][curturn]][y][x])
+                {
+                    bricks[line + y][xpos + x] = 0;
+                }
+            }
+        }
+
         line++;
+    }
+    else
+    {
+        return;
+    }
+
+    for (int y = 0; y < 4; y++)
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            if (tetromino[tetIndex[curblock][curturn]][y][x])
+            {
+                bricks[line + y][xpos + x] = tetromino[tetIndex[curblock][curturn]][y][x];
+            }
+        }
     }
 }
 
 void land(void)
 {
-
+    //line = 0;
+    //xpos = 3;
 }
 
 void rotate(void)
